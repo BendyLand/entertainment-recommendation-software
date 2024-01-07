85faa88cdbd2785001f38cc0e5f24d7dc475f4ed
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-01-07 11:07:26 -0500

    Think of an idea for the project and update Kanban.

---
+++ script.py
@@ -0,0 +1,11 @@
+""" 
+Idea Planning:
+? What kind of recommendation system do you want to build?
+I will build an entertainment recommendation system. 
+
+? Who do you want to build something for? Yourself? Your friends? Your family? 
+I want to build something that addresses an annoyance of mine, which hopefully would become something that others would want to use.
+
+? What topics will be searchable in your recommendation system?
+Users can input general information such as a media type as well as guidance information, such as a decade or a genre, and the system will return a recommendation of some form of entertainment.
+"""
