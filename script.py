14e9b203b0ee4d7b7fc540c1464effec15db136a
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-01-08 19:52:53 -0500

    Start brainstorming project details

--- script.py
+++ script.py
@@ -9,3 +9,16 @@ I want to build something that addresses an annoyance of mine, which hopefully w
 ? What topics will be searchable in your recommendation system?
 Users can input general information such as a media type as well as guidance information, such as a decade or a genre, and the system will return a recommendation of some form of entertainment.
 """
+
+""" 
+Project Brainstorming:
+? What does your program do?
+My program will take in general information to help you narrow down a form of entertainment to consume.
+? What data do you need?
+I need a large collection of various pieces of entertainment; songs, movies, tv shows, games, etc. 
+? What questions will you ask the user?
+Guiding questions to help narrow down the type of entertainment the user is looking for
+
+? How do the above questions combine to return a recommendation?
+! Make sure it satisfies all the project requirements
+"""
