e1278c102b721516d7003d5f70a9e8345e99eb11
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-01-10 08:52:04 -0500

    Add movies to media file

--- media.py
+++ media.py
@@ -107,8 +107,153 @@ games = {
 }
 
 movies = {
-    
+    "Raiders of the Lost Ark": {
+        "year": 1981,
+        "genre": ["action-adventure", "fantasy"],
+        "length": 115 # minutes
+    },
+    "Excalibur": {
+        "year": 1981,
+        "genre": ["medieval", "fantasy", "action-adventure"],
+        "length": 119
+    },
+    "Spaceballs": {
+        "year": 1987,
+        "genre": ["comedy", "parody", "sci-fi"],
+        "length": 96
+    },
+    "Gremlins": {
+        "year": 1984,
+        "genre": ["horror", "comedy", "kid-friendly"],
+        "length": 106
+    },
+    "Leviathan": {
+        "year": 1989,
+        "genre": ["sci-fi", "horror", "thriller"],
+        "length": 98
+    },
+    "The Island": {
+        "year": 1980,
+        "genre": ["action-adventure", "thriller"],
+        "length": 114
+    },
+    "Armageddon": {
+        "year": 1998,
+        "genre": ["sci-fi", "action-adventure"],
+        "length": 153
+    },
+    "The Mask": {
+        "year": 1994,
+        "genre": ["comedy", "fantasy", "action-adventure"],
+        "length": 101
+    },
+    "Big Daddy": {
+        "year": 1999,
+        "genre": ["comedy"],
+        "length": 94
+    },
+    "The Game": {
+        "year": 1997,
+        "genre": ["mystery", "thriller"],
+        "length": 129
+    },
+    "The Silence of the Lambs": {
+        "year": 1991,
+        "genre": ["horror", "thriller"],
+        "length": 118
+    },
+    "Beethoven": {
+        "year": 1992,
+        "genre": ["comedy", "kid-friendly"],
+        "length": 87
+    },
+    "The Da Vinci Code": {
+        "year": 2006,
+        "genre": ["mystery", "thriller"],
+        "length": 149
+    },
+    "300": {
+        "year": 2006,
+        "genre": ["action-adventure", "historical"],
+        "length": 117
+    },
+    "The Dark Knight": {
+        "year": 2008,
+        "genre": ["action-adventure", "superhero"],
+        "length": 152
+    },
+    "Spider-Man 3": {
+        "year": 2007,
+        "genre": ["superhero", "action-adventure"],
+        "length": 139
+    },
+    "The Butterfly Effect": {
+        "year": 2004,
+        "genre": ["sci-fi", "thriller"],
+        "length": 120
+    },
+    "Click": {
+        "year": 2006,
+        "genre": ["comedy", "fantasy"],
+        "length": 107
+    },
+    "Sky High": {
+        "year": 2005,
+        "genre": ["superhero", "comedy", "kid-friendly"],
+        "length": 100
+    },
+    "Rise of the Planet of the Apes": {
+        "year": 2011,
+        "genre": ["action-adventure", "sci-fi"],
+        "length": 105
+    },
+    "Interstellar": {
+        "year": 2014,
+        "genre": ["sci-fi", "thriller"],
+        "length": 167
+    },
+    "Spider-Man: Into the Spider-Verse": {
+        "year": 2018,
+        "genre": ["superhero", "animated", "comedy", "kid-friendly"],
+        "length": 116
+    },
+    "Ready Player One": {
+        "year": 2018,
+        "genre": ["sci-fi", "action-adventure"],
+        "length": 140
+    }
 }
 
-tv_shows = {}
+# tv_shows = {
+#     "Saved by the Bell": {
+#         "year": 1989,
+#         "genre": ["sitcom", "kid-friendly"],
+#         "length": 86 # episodes
+#     },
+#     "Cops": {
+#         "year": 1989,
+#         "genre": ["action", "crime"],
+#         "length": 1100
+#     },
+#     "Full House": {
+#         "year": 1987,
+#         "genre": ["sitcom", "kid-friendly"],
+#         "length": 192
+#     },
+#     "The Simpsons": {
+#         "year": 1989,
+#         "genre": ["animated", "sitcom"],
+#         "length": 760
+#     },
+#     "Seinfeld": {
+#         "year": 1989,
+#         "genre": ["sitcom"],
+#         "length": 180
+#     },
+#     "Star Trek: Voyager": {
+#         "year": 1995,
+#         "genre": []
+#     }
+# }
+
 songs = {}
