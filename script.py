5ffbd3774cfe46518696509e7649aff2d1e18f34
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-01-09 19:22:39 -0500

    Continue planning and write initial print statement

--- script.py
+++ script.py
@@ -22,3 +22,20 @@ Guiding questions to help narrow down the type of entertainment the user is look
 ? How do the above questions combine to return a recommendation?
 ! Make sure it satisfies all the project requirements
 """
+
+# It might be good to have a list of three options as a result.
+
+# To get to that point, find out what the user wants to prioritize (media, date, etc.) 
+# then narrow it down from that point. 
+
+# Use letters for user input a) action, b) action, etc.
+
+print(
+"""\
+Welcome to the Entertainment Recommendation System!
+
+We have a vast collection of media to share,
+but in order to give you a recommendation,
+we need to narrow down your choices.
+""")
+
