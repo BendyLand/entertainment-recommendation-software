c5a6ee7a7473e07d4e3a42cca84873f69e27c10c
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-01-09 20:59:40 -0500

    Organize code into functions and create dictionaries for media

--- script.py
+++ script.py
@@ -1,16 +1,10 @@
+from textwrap import dedent
 """ 
 Idea Planning:
-? What kind of recommendation system do you want to build?
-I will build an entertainment recommendation system. 
-
-? Who do you want to build something for? Yourself? Your friends? Your family? 
-I want to build something that addresses an annoyance of mine, which hopefully would become something that others would want to use.
-
 ? What topics will be searchable in your recommendation system?
 Users can input general information such as a media type as well as guidance information, such as a decade or a genre, and the system will return a recommendation of some form of entertainment.
-"""
+---------------------------
 
-""" 
 Project Brainstorming:
 ? What does your program do?
 My program will take in general information to help you narrow down a form of entertainment to consume.
@@ -25,17 +19,27 @@ Guiding questions to help narrow down the type of entertainment the user is look
 
 # It might be good to have a list of three options as a result.
 
-# To get to that point, find out what the user wants to prioritize (media, date, etc.) 
+# To get to that point, find out what the user wants to prioritize (media, date, length, feel, etc.) 
 # then narrow it down from that point. 
 
 # Use letters for user input a) action, b) action, etc.
 
-print(
-"""\
-Welcome to the Entertainment Recommendation System!
+games = {}
+movies = {}
+tv_shows = {}
+songs = {}
 
-We have a vast collection of media to share,
-but in order to give you a recommendation,
-we need to narrow down your choices.
-""")
+def greet():
+    print(dedent("""\
+        Welcome to the Entertainment Recommendation System!
 
+        We have a vast collection of media to share,
+        but in order to give you a recommendation,
+        we need to narrow down your choices.
+    """))
+
+def start():
+    greet()
+
+
+start()
